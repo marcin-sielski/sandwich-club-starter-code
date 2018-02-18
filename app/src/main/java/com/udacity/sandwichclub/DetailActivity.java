9163b3cf4421018a9a07bf5006ef7243cd6b9d9e
Marcin Sielski <marcin.sielski@gmail.com>
2018-02-18 17:34:43 +0100

    Project completion.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,19 +1,27 @@
 package com.udacity.sandwichclub;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +50,11 @@ public class DetailActivity extends AppCompatActivity {
             closeOnError();
             return;
         }
-
-        populateUI();
+        if (!isNetworkAvailable()) {
+            closeOnError();
+            return;
+        }
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +67,62 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView alsoKnownAsTv = findViewById(R.id.also_known_tv);
+        List<String> alsoKnownAs = sandwich.getAlsoKnownAs();
+        StringBuffer alsoKnownAsText = new StringBuffer();
+        for (int index = 0; index < alsoKnownAs.size(); index++) {
+            alsoKnownAsText.append(alsoKnownAs.get(index));
+            if (index < alsoKnownAs.size() - 1) {
+                alsoKnownAsText.append(", ");
+            }
+        }
+        if (alsoKnownAs.size() > 0) {
+            alsoKnownAsTv.setText(alsoKnownAsText);
+        } else {
+            findViewById(R.id.also_known_as_label).setVisibility(View.GONE);
+            findViewById(R.id.also_known_tv).setVisibility(View.GONE);
+        }
 
+        TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+        List<String> ingredients = sandwich.getIngredients();
+        StringBuffer ingredientsText = new StringBuffer();
+        for (int index = 0; index < ingredients.size(); index++) {
+            ingredientsText.append(ingredients.get(index));
+            if (index < ingredients.size() - 1) {
+                ingredientsText.append(", ");
+            }
+        }
+        if (ingredientsText.length() > 0) {
+            ingredientsTv.setText(ingredientsText);
+        } else {
+            findViewById(R.id.ingredients_label).setVisibility(View.GONE);
+            findViewById(R.id.ingredients_tv).setVisibility(View.GONE);
+        }
+
+        TextView placeOfOriginTv = findViewById(R.id.origin_tv);
+        String placeOfOriginText = sandwich.getPlaceOfOrigin();
+        if (placeOfOriginText.length() > 0) {
+            placeOfOriginTv.setText(placeOfOriginText);
+        } else {
+            findViewById(R.id.place_of_origin_label).setVisibility(View.GONE);
+            findViewById(R.id.origin_tv).setVisibility(View.GONE);
+        }
+
+        TextView descriptionTv = findViewById(R.id.description_tv);
+        String descriptionText = sandwich.getDescription();
+        if (descriptionText.length() > 0) {
+            descriptionTv.setText(descriptionText);
+        } else {
+            findViewById(R.id.description_label).setVisibility(View.GONE);
+            findViewById(R.id.description_tv).setVisibility(View.GONE);
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,46 @@
 package com.udacity.sandwichclub.utils;
 
+import android.support.annotation.NonNull;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = null;
+        try {
+            JSONObject jsonSandwich = new JSONObject(json);
+            JSONObject jsonName = jsonSandwich.getJSONObject("name");
+            String mainName = jsonName.getString("mainName");
+            JSONArray jsonAlsoKnownAs = jsonName.getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAs = new ArrayList<>();
+            for (int index = 0; index < jsonAlsoKnownAs.length(); index++) {
+                alsoKnownAs.add(jsonAlsoKnownAs.getString(index));
+            }
+            String placeOfOrigin = jsonSandwich.getString("placeOfOrigin");
+            String description = jsonSandwich.getString("description");
+            String image = jsonSandwich.getString("image");
+            JSONArray jsonIngredients = jsonSandwich.getJSONArray("ingredients");
+            List<String> ingredients = new ArrayList<>();
+            for (int index = 0; index < jsonIngredients.length(); index ++) {
+                ingredients.add(jsonIngredients.getString(index));
+            }
+
+            sandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image,
+                    ingredients);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sandwich;
     }
 }
